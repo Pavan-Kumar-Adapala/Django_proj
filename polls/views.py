ec1d6bbf3c5332f8c764872f83f4e61b8c0d9713
Pavan-Kumar-Adapala <pavankumar.adapala.msc@gmail.com>
2024-12-19 09:59:08 +0100

    added flake8 file

--- polls/views.py
+++ polls/views.py
@@ -1,51 +1,8 @@
-#from django.http import HttpResponse, HttpResponseRedirect
-#from django.template import loader
-
-#from django.shortcuts import render   # used in index,detail functions
-#from django.http import Http404       # used in detail function to raise exception
-
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
 from django.views import generic
-
 from .models import Question, Choice
-
-"""
-def index(request):
-    new_que_list = Question.objects.order_by('-Pub_date')[:5]
-    # the below "output" writen without template
-    '''output = ' , '.join([q.question_tpy for q in new_que_list])  #question_tpy in models.py
-    return HttpResponse(output)'''
-    # here we are using template (the path is templates/polls/index.html)
-    '''template = loader.get_template('polls/index.html')
-    context = {
-        'new_que_list': new_que_list,
-    }
-    return HttpResponse(template.render(context, request))'''
-
-    # we can write in simple way by using only "render()"
-    
-    context = {'new_que_list': new_que_list,}
-    return render(request, 'polls/index.html', context)    # for we need import render from django.shortcuts
-
-def detail(request, question_id): 
-    '''
-    try:
-        question = Question.objects.get(pk=question_id)    
-    except Question.DoesNotExist:
-        raise Http404("Question Does Not Exist")
-    return render(request, 'polls/detail.html', {'question': question})'''
-
-    # the above code writen in simple way by using "get_object_or_404()"
-
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {'question':question})
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {'question': question} 
-"""
 
 # using class based views and also generic views for short and simple code.
 class IndexView(generic.ListView):
